5af4891701afc1abd98dd12c969deee44f992505
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-05-23 18:41:33 -0700

    not passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    i = 1
+    num = 0
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    elif length > 2:
+        while i < length:
+            num_list = [0, 1]
+            num_list.append(num_list[i]+num_list[i-1])
+            i+=1
+        print (num_list)
